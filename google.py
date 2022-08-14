efeef3c27d392542f2be5d7ae44bbdebcf29dae4
ownvoy <wonjun703@naver.com>
2022-08-15 05:25:44 +0900

    크롤링, 장고 연결

--- google.py
+++ google.py
@@ -1,18 +1,27 @@
 from urllib.parse import quote_plus
-
+from search.models import Data
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-init_url = "https://www.google.com/search?q="
-search_term = input("Search: ")
-url = init_url + quote_plus(search_term)
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DASF002.settings")
 
-driver = webdriver.Chrome()
-driver.get(url)
+import django
+django.setup()
 
-for x in driver.find_elements(By.CLASS_NAME, "yuRUbf"):
-    aTag = x.find_element(By.TAG_NAME, "a")
-    aH3 = x.find_element(By.TAG_NAME, "h3")
-    href = aTag.get_attribute("href")
-    print("제목: ", aH3.text)
-    print("링크: ", href)
+class Chrome:
+    def googleCrawling(search_term):
+        init_url = "https://www.google.com/search?q="
+        url = init_url + quote_plus(search_term)
+
+        driver = webdriver.Chrome()
+        driver.get(url)
+        data = []
+        for x in driver.find_elements(By.CLASS_NAME, "yuRUbf"):
+            atag = x.find_element(By.TAG_NAME, "a")
+            h3 = x.find_element(By.TAG_NAME, "h3")
+            href = atag.get_attribute("href")
+            discription = "asfdasf"
+            data.append([h3.text, href,discription])
+        return data
+
